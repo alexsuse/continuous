5312b46725575cd5e037d6b18c00a02d2806cde9
alexsuse <alexsusemihl@gmail.com>
2013-08-27 09:24:30 +0200

    yep

---
+++ estimation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+'''
+finds the optimal encoder for the filtering problem.
+'''
+
+import numpy as np
+import scipy.optimize as opt
+
+def d_eps_dt(eps,gamma,eta,alpha,lamb):
+	return -2*gamma*eps+eta**2-lamb*eps**2/(alpha**2+eps)
+	#return -np.dot(gamma,eps)-np.dot(eps,gamma.T)+np.dot(eta.T,eta)-lamb*np.linalg.solve(alpha+eps,np.dot(eps,eps))
+
+def get_eq_eps(gamma,eta,alpha,lamb):
+	f = lambda e : d_eps_dt(e,gamma,eta,alpha,lamb)
+	return opt.fsolve(f,1.0)
